cc67e45b59883925faacb82880bb291993190bda
Aakash Gupta <gupta29470@gmail.com>
2021-01-20 22:50:25 +0530

    auth file with 2 text fields

---
+++ lib/auth.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class Auth extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Git and Flutter"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Container(
+          child: SingleChildScrollView(
+            child: Container(
+              child: Center(
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: "email",
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: "email",
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
