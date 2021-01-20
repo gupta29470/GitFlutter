b6c594e9f24a2e6520b1390352cea3fb2a3c56c4
Aakash Gupta <gupta29470@gmail.com>
2021-01-20 22:44:14 +0530

    text field added

--- lib/main.dart
+++ lib/main.dart
@@ -6,32 +6,33 @@ void main(){
   ));
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _State createState() => new _State();
-}
-
-//State is information of the application that can change over time or when some actions are taken.
-class _State extends State<MyApp>{
-
-  double _value = 0.0;
-  void _setvalue(double value) => setState(() => _value = value);
-
+class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('Name here'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Git and Flutter"),
+        centerTitle: true,
       ),
-      //hit Ctrl+space in intellij to know what are the options you can use in flutter widgets
-      body: new Container(
-        padding: new EdgeInsets.all(32.0),
-        child: new Center(
-          child: new Column(
-            children: <Widget>[
-              new Text('Value: ${(_value * 100).round()}'),
-              new Slider(value: _value, onChanged: _setvalue)
-            ],
+      body: Center(
+        child: Container(
+          child: SingleChildScrollView(
+            child: Container(
+              child: Center(
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: "email",
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ),
         ),
       ),
